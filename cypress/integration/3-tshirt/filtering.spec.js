b8f681e7123eadabd9774e9d0c61175fe3234998
Juan Diego Coronel <jdcm95@gmail.com>
2022-01-26 22:06:10 +0100

    Purchase product and Filtering by Size tests added

---
+++ cypress/integration/3-tshirt/filtering.spec.js
@@ -0,0 +1,48 @@
+describe('Filter products by size', () => {
+    beforeEach(() => {
+        cy.visit('localhost:3000')
+        cy.wait(2000)
+        cy.get('[class=products-found]').invoke('text').as('txt')
+    })
+
+    it('Filter by XS size', () => {
+        cy.get('[class=checkmark]').contains('XS').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+    })
+
+    it('Filter by M size', () => {
+        cy.get('[class=checkmark]').contains('M').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+        cy.wait(2000)
+    })
+
+    it('Filter by ML size', () => {
+        cy.get('[class=checkmark]').contains('ML').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+        cy.wait(2000)
+    })
+
+    it('Filter by L size', () => {
+        cy.get('[class=checkmark]').contains('L').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+        cy.wait(2000)
+    })
+
+    it('Filter by XL size', () => {
+        cy.get('[class=checkmark]').contains('XL').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+        cy.wait(2000)
+    })
+
+    it('Filter by XXL size', () => {
+        cy.get('[class=checkmark]').contains('XXL').click()
+        cy.wait(2000)
+        cy.get('[class=products-found]').should('not.have.text', '@txt')
+        cy.wait(2000)
+    })
+})
